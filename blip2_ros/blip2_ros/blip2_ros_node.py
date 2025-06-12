b2cf58b784700fc805f884090f38b03cd544bf02
KevinEppacher <kevin-eppacher@hotmail.de>
2025-06-12 15:03:27 +0200

    Renaming

--- blip2_ros/blip2_ros/blip2_ros_node.py
+++ blip2_ros/blip2_ros/blip2_ros_node.py
@@ -1,5 +1,5 @@
 import rclpy
-from blip2_ros.ros2_wrapper.Blib2LifecycleNode import Blip2Node
+from blip2_ros.ros2_wrapper.blip2_ros import Blip2Node
 
 def main(args=None):
     rclpy.init(args=args)
